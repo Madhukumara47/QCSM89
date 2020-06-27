6d30cb18b71131982e6da03663f9a9f9f7930cf8
Madhu <Madhu@DESKTOP-863M11C>
2020-06-27 19:55:49 +0530

    My First Project into github

---
+++ RAS/src/test/java/com/ras/testscript/TestFramework.java
@@ -0,0 +1,17 @@
+package com.ras.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestFramework
+	{
+		@Test
+		public void tc_01()
+		{
+			Reporter.log("First testcase " , true);
+		}
+	
+		
+	}
+
+
